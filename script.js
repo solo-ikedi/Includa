07ca32094047b6334fe73ca0c1284ff50ee51932
Ubani Solomon Ikedi <155390861+solo-ikedi@users.noreply.github.com>
2025-05-06 15:48:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours();
+
+if (hour < 12) {
+  greeting.textContent = "Good Morning!";
+} else if (hour < 18) {
+  greeting.textContent = "Good Afternoon!";
+} else {
+  greeting.textContent = "Good Evening!";
+}
